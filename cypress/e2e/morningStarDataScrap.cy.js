54d3f4a934c34eabe19fef9ba550275df59d698a
Turner-44 <matty44@windowslive.com>
2022-12-12 16:54:31 +1300

    Adding some error handling

--- cypress/e2e/morningStarDataScrap.cy.js
+++ cypress/e2e/morningStarDataScrap.cy.js
@@ -6,39 +6,50 @@ describe("morningStarGetPageValues", () => {
       return false;
     });
 
+    cy.intercept("**/financials").as("stockData");
+
     A_D.forEach((stock) => {
       cy.visit(`https://www.morningstar.com/stocks/xnys/${stock}/financials`, {
         timeout: 20000,
+        failOnStatusCode: false,
       });
 
-      cy.intercept("GET", "**/incomeStatement/detail?*").as("incomeStatement");
-      cy.intercept("GET", "**/balanceSheet/detail?*").as(
-        "balanceSheetStatement"
-      );
-      cy.intercept("GET", "**/cashFlow/detail?*").as("cashFlowStatement");
+      cy.wait("@stockData").then((req) => {
+        if (req.response.statusCode == 200) {
+          cy.intercept("GET", "**/incomeStatement/detail?*").as(
+            "incomeStatement"
+          );
+          cy.intercept("GET", "**/balanceSheet/detail?*").as(
+            "balanceSheetStatement"
+          );
+          cy.intercept("GET", "**/cashFlow/detail?*").as("cashFlowStatement");
 
-      cy.contains("Expand Detail View").click();
-      cy.wait("@incomeStatement").then((req) => {
-        cy.writeFile(
-          `outputs/nyse/A_D/incomeStatment/${stock}.json`,
-          JSON.stringify(req.response.body)
-        );
-      });
+          cy.contains("Expand Detail View", { timeout: 10000 }).click();
+          cy.wait("@incomeStatement").then((req) => {
+            cy.writeFile(
+              `outputs/xyse/A_D/incomeStatment/${stock}.json`,
+              JSON.stringify(req.response.body)
+            );
+          });
 
-      cy.get("#balanceSheet").click();
-      cy.wait("@balanceSheetStatement").then((req) => {
-        cy.writeFile(
-          `outputs/nyse/A_D/balanceSheet/${stock}.json`,
-          JSON.stringify(req.response.body)
-        );
-      });
+          cy.get("#balanceSheet").click();
+          cy.wait("@balanceSheetStatement").then((req) => {
+            cy.writeFile(
+              `outputs/xyse/A_D/balanceSheet/${stock}.json`,
+              JSON.stringify(req.response.body)
+            );
+          });
 
-      cy.get("#cashFlow").click();
-      cy.wait("@cashFlowStatement").then((req) => {
-        cy.writeFile(
-          `outputs/nyse/A_D/cashFlow/${stock}.json`,
-          JSON.stringify(req.response.body)
-        );
+          cy.get("#cashFlow").click();
+          cy.wait("@cashFlowStatement").then((req) => {
+            cy.writeFile(
+              `outputs/xyse/A_D/cashFlow/${stock}.json`,
+              JSON.stringify(req.response.body)
+            );
+          });
+        } else {
+          cy.writeFile(`outputs/xyse/A_D/failures/${stock}.text`, "");
+        }
       });
     });
   });
